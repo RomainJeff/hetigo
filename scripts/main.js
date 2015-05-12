ec11674937b5567a53853421ba79acd664ed7bc3
AudeFi <odefi@hotmail.fr>
2015-05-12 11:34:52 +0200

    Declaration variables
    Declaration des variables à utiliser

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+/*
+
+var game[][][];
+
+// 1ere et 2 eme dimensions = plateau de jeu actuel (mis à jour à chaque tour)
+// 3 eme dimensio, enregistre les pions à chaque tour (1pion par case)
+// etatJeu[x][y][0] = actualisé à chaque fois, toutp plateau enregistré
+// etatJeu[x][y][1], etatJeu[x][y][2], etatJeu[x][y][tour] etc. = pion joué enregsitré à chaque fois
+
+var tour = 1;
+
+var group[][]; // Group of each cell
+
+var prisonnier;
+
+
+function 
+
+
+*/
